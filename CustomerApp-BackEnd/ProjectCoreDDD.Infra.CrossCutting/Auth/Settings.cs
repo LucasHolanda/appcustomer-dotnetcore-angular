c0340dd134062ddfa89c981f0b4b034ba04ba915
Lucas Holanda <luckas.holanda@gmail.com>
2021-02-11 17:30:12 -0400

    Correcao de pastas e caminhos!

--- CustomerApp-BackEnd/ProjectCoreDDD.Infra.CrossCutting/Auth/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjectCoreDDD.Infra.CrossCutting.Auth
-{
-    public static class Settings
-    {
-        //CustomerManagement@123
-        public static string Secret = "65a6afe76421afe35bd558a5f11d4f61";
-    }
-}
